878fcc6a48429e73e279c484993d6cbe4eb5addb
StefLinde <StefLinde@users.noreply.github.com>
2018-02-28 21:00:28 +0100

    Simple version

--- include/SimpleEstimator.h
+++ include/SimpleEstimator.h
@@ -8,17 +8,29 @@
 #include "Estimator.h"
 #include "SimpleGraph.h"
 
+struct PathElement {
+    char* direction;
+    uint32_t label;
+};
+
+
 class SimpleEstimator : public Estimator {
 
     std::shared_ptr<SimpleGraph> graph;
 
+    std::unordered_map<uint32_t, uint32_t> starts, paths, ends;
+
+    cardStat join(RPQTree *q, cardStat l, cardStat r);
+    bool nonEmpty(cardStat s);
+    PathElement getPathElement(RPQTree *q);
+
 public:
     explicit SimpleEstimator(std::shared_ptr<SimpleGraph> &g);
+
     ~SimpleEstimator() = default;
-
     void prepare() override ;
-    cardStat estimate(RPQTree *q) override ;
 
+    cardStat estimate(RPQTree *q) override ;
 };
 
 
--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -12,14 +12,60 @@ SimpleEstimator::SimpleEstimator(std::shared_ptr<SimpleGraph> &g){
 }
 
 void SimpleEstimator::prepare() {
+    for (uint32_t vertex = 0; vertex < graph->getNoVertices(); ++vertex) {
+        for (auto out : graph->adj[vertex]) {
+            const auto label = out.first;
+            starts[label]++;
+            paths[label]++;
+        }
 
-    // do your prep here
-
+        for (auto in : graph->reverse_adj[vertex]) {
+            const auto label = in.first;
+            ends[label]++;
+        }
+    }
 }
 
 cardStat SimpleEstimator::estimate(RPQTree *q) {
+    //If the query is divisible, first estimate the children and then combine
+    if (q->isConcat()) {
+        cardStat l = estimate(q->left);
+        cardStat r = estimate(q->right);
+        return join(q, l, r);
+    }
 
-    // perform your estimation here
+    PathElement pathElement = getPathElement(q);
 
-    return cardStat {0, 0, 0};
+    if (*pathElement.direction == '+') {
+        return cardStat {starts[pathElement.label],
+                         paths[pathElement.label],
+                         ends[pathElement.label]};
+    } else {
+        return cardStat {ends[pathElement.label],
+                         paths[pathElement.label],
+                         starts[pathElement.label]};
+    }
+}
+
+cardStat SimpleEstimator::join(RPQTree *q, cardStat l, cardStat r) {
+    if (!nonEmpty(l) || !nonEmpty(r)) {
+        return cardStat {0, 0, 0};
+    }
+
+    uint32_t in = l.noIn;
+    uint32_t out = r.noOut;
+    auto paths = l.noPaths + r.noPaths;
+
+    return cardStat {in, paths, out};
+}
+
+bool SimpleEstimator::nonEmpty(cardStat s) {
+    return s.noIn != 0 && s.noPaths != 0 && s.noOut != 0;
+}
+
+PathElement SimpleEstimator::getPathElement(RPQTree *q) {
+    PathElement pathElement = PathElement();
+    pathElement.label = static_cast<uint32_t>(strtoll(q->data.c_str(), &pathElement.direction, 0));
+
+    return pathElement;
 }
